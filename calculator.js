97546e60ca034bb907eaa3f4a57d4bc8b264c635
“Rebecca <rebeccaschiffman@gmail.com>
2022-11-25 20:24:29 -0800

    Try to make calculator function into object

--- calculator.js
+++ calculator.js
@@ -1,13 +1,38 @@
-function calculator(a,b, op) {
-  if (op === 'sum') {
-    return a + b;
-  } else if (op === 'subtract') {
-    return a - b;
-  } else if (op === 'multiply') {
-    return a * b;
-  } else if (op === 'divide') {
-    return a/b;
-  }
-}
+// function calculator(a,b, op) {
+//   if (op === 'sum') {
+//     return a + b;
+//   } else if (op === 'subtract') {
+//     return a - b;
+//   } else if (op === 'multiply') {
+//     return a * b;
+//   } else if (op === 'divide') {
+//     return a/b;
+//   }
+// }
 
-export default calculator; 
+const Calculator = function(a, b, op) {
+
+  const sum = () => a + b;
+  const subtract = () => a - b;
+  const multiply = () => a * b;
+  const divide = () => a / b;
+
+  let result = '';
+  if (op === 'sum') {
+    result = sum
+  } else if (op === 'subtract') {
+    result = subtract;
+  } else if (op === 'multiply') {
+    result = multiply;
+  } else if (op === 'divide') {
+    result = divide;
+  }
+
+  return { result};
+
+};
+
+
+
+// export default calculator; 
+// export default Calculator; 
--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,21 @@
-import calculator from './calculator'; 
+import Calculator from './calculator'; 
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(calculator(1,2,'sum')).toBe(3);
+  expect(Calculator(1,2,'sum')).toBe(3);
 });
 
-test('5 - 2 to equal 3', () => {
-  expect(calculator(5,2,'subtract')).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(calculator(1,2,'sum')).toBe(3);
+// });
 
-test('5 * 2 to equal 10', () => {
-  expect(calculator(5,2,'multiply')).toBe(10);
-});
+// test('5 - 2 to equal 3', () => {
+//   expect(calculator(5,2,'subtract')).toBe(3);
+// });
 
-test('64 / 2 to equal 32', () => {
-  expect(calculator(64,2,'divide')).toBe(32);
-});
+// test('5 * 2 to equal 10', () => {
+//   expect(calculator(5,2,'multiply')).toBe(10);
+// });
+
+// test('64 / 2 to equal 32', () => {
+//   expect(calculator(64,2,'divide')).toBe(32);
+// });
